326385968785d35f92db5a0e4cd889df50f33203
Vladosik1963 <Vladmalinin1999@gmail.com>
2021-01-05 15:46:54 +0200

    Create pagination,add search in post,Now you can create post in website and it wtill be add in DB

--- app/models.py
+++ app/models.py
@@ -8,6 +8,13 @@ def slugify(s):
     return re.sub(pattern, '-', s)
 
 
+#Relationship Table
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
@@ -19,9 +26,25 @@ class Post(db.Model):
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic')) # Lazy = Return type 'posts'
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
 
+#Watch in console
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}>'
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100), unique=True)  # url
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Tag id: {self.id}, title: {self.name}>'
--- app/view.py
+++ app/view.py
@@ -4,10 +4,6 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    '''
-    Main page "/"
-    '''
 
-    name = 'Vlad'
-    return render_template('index.html', n=name)
+    return render_template('index.html')
 
